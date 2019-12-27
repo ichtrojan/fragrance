cb5405078a232a1efbb54779eed64d5df5fe3fcf
ichtrojan <ordrizzy@gmail.com>
2019-12-27 11:53:44 +0100

    The main plug: Init the seeder

--- main.go
+++ main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
-	"github.com/ichtrojan/fragrance/routes"
 	"log"
 	"net/http"
+
+	"github.com/ichtrojan/fragrance/database"
+	"github.com/ichtrojan/fragrance/routes"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("No .env file found")
+	}
+
+	database.SeedAdmin()
+
 	if err := http.ListenAndServe(":9000", routes.Init()); err != nil {
 		log.Fatal(err)
 	}
