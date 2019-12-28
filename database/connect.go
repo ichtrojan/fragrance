e08cb7df54791888e3404cfe46b6cab07e069886
ichtrojan <ordrizzy@gmail.com>
2019-12-28 12:30:02 +0100

    Modifies to allow custom DB host

--- database/connect.go
+++ database/connect.go
@@ -22,13 +22,19 @@ func Init() *gorm.DB {
 		log.Fatal("DB_PASS not set in .env")
 	}
 
+	host, exist := os.LookupEnv("DB_HOST")
+
+	if !exist {
+		log.Fatal("DB_HOST not set in .env")
+	}
+
 	name, exist := os.LookupEnv("DB_NAME")
 
 	if !exist {
 		log.Fatal("DB_NAME not set in .env")
 	}
 
-	credentials := fmt.Sprintf("%s:%s@/%s?charset=utf8&parseTime=True&loc=Local", user, pass, name)
+	credentials := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?charset=utf8&parseTime=True&loc=Local", user, pass, host, name)
 
 	db, err := gorm.Open("mysql", credentials)
 
