1a4d16d708c5198111a58043a6dd09ecaa621704
ichtrojan <ordrizzy@gmail.com>
2019-12-30 14:40:10 +0100

    Category Seeder

--- database/seeder.go
+++ database/seeder.go
@@ -23,6 +23,36 @@ func SeedAdmin() {
 	db.Create(&admin)
 }
 
+func SeedCategory() {
+	db := Init()
+
+	db.DropTable(&models.Category{})
+
+	db.AutoMigrate(&models.Category{})
+
+	categories := []models.Category{
+		{
+			Name:  "Feminine",
+			Slug:  "feminine",
+			Image: "https://res.cloudinary.com/ichtrojan/image/upload/v1577708397/fragrance/feminine-bg.jpg",
+		},
+		{
+			Name:  "Masculine",
+			Slug:  "masculine",
+			Image: "https://res.cloudinary.com/ichtrojan/image/upload/v1577708255/fragrance/masculine-bg.jpg",
+		},
+		{
+			Name:  "Unisex",
+			Slug:  "unisex",
+			Image: "https://res.cloudinary.com/ichtrojan/image/upload/v1577708407/fragrance/unisex-bg.jpg",
+		},
+	}
+
+	for _, category := range categories {
+		db.Create(&category)
+	}
+}
+
 func hashAndSalt(password []byte) string {
 	hash, err := bcrypt.GenerateFromPassword(password, bcrypt.MinCost)
 
